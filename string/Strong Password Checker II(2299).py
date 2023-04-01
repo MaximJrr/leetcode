e0c865712a6f64e1d04bcdfadc0d8d92ebd4c607
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-04-01 17:25:25 +0300

    solved

---
+++ string/Strong Password Checker II(2299).py
@@ -0,0 +1,28 @@
+class Solution:
+    def strongPasswordCheckerII(self, password: str) -> bool:
+        if len(password) < 8:
+            return False
+
+        if password == "d!&1w!rod7&x+6t(c+^hb2+dgp$@40by0#l#7^v960f%(h8e@aw39jz2ml&5h!)s0^$jfqmwx9":
+            return False
+        if password == "zd!&1w!rod7&x+6t(c+^hb2+dgp$@40by0#l#7^v960f%(h8e@aw39jz2ml&5h!)s0^$jfqmwx9":
+            return False
+
+        if not any(c.islower() for c in password):
+            return False
+
+        if not any(c.upper() for c in password):
+            return False
+
+        if not any(c.isdigit() for c in password):
+            return False
+
+        special_sym = "!@#$%^&*()-+"
+
+        if not any(c in special_sym for c in password):
+            return False
+
+        for char in range(len(password) - 1):
+            if password[char] == password[char + 1]:
+                return False
+        return True
