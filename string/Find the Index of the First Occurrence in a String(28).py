9573fd1b37f3995c2245aa7319979b818a055d39
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-04-02 14:16:55 +0300

    solved

--- string/Find the Index of the First Occurrence in a String(28).py
+++ string/Find the Index of the First Occurrence in a String(28).py
@@ -1,10 +1,8 @@
-def strStr(haystack: str, needle: str) -> int:
-    if needle == "":
-        return 0
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
 
-    if needle in haystack:
-        return haystack.index(needle)
-    return -1
-
-
-print(strStr(haystack = "leetcode", needle = "leeto"))
+        if needle in haystack:
+            return haystack.index(needle)
+        return -1
