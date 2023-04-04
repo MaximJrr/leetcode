2acb2b72e5918446f8d23edb9bbae20d26d7c900
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-04-04 11:38:05 +0300

    solved

--- string/Minimum Index Sum of Two Lists(599).py
+++ string/Minimum Index Sum of Two Lists(599).py
@@ -1,26 +1,23 @@
-def findRestaurant(list1, list2):
-    hash_list1 = {}
-    result = []
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        hash_list1 = {}
+        result = []
 
-    for index, word in enumerate(list1):
-        if word not in list2:
-            continue
-        if word in list2:
-            hash_list1[word] = index
+        for index, word in enumerate(list1):
+            if word not in list2:
+                continue
+            if word in list2:
+                hash_list1[word] = index
 
-    for index, word in enumerate(list2):
-        if word not in list1:
-            continue
-        if word in list1:
-            hash_list1[word] += index
+        for index, word in enumerate(list2):
+            if word not in list1:
+                continue
+            if word in list1:
+                hash_list1[word] += index
 
-    min_value = min(hash_list1.values())
+        min_value = min(hash_list1.values())
 
-    for word in hash_list1:
-        if hash_list1[word] == min_value:
-            result.append(word)
-    return result[::-1]
-
-
-
-print(findRestaurant(list1 = ["happy","sad","good"], list2 = ["sad","happy","good"]))
+        for word in hash_list1:
+            if hash_list1[word] == min_value:
+                result.append(word)
+        return result[::-1]
