d4ab43bfee492645de4193e5f4ef43f579a4a4ce
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-04-14 10:23:20 +0300

    solved

--- array/Squares of a Sorted Array(997).py
+++ array/Squares of a Sorted Array(997).py
@@ -1,19 +1,4 @@
-import random
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted([i ** 2 for i in nums])
 
-
-def flipAndInvertImage(image):
-    matrix = [[0 for j in range(3)] for i in range(len(image))]
-    matrix_pointer, image_pointer = 0, 0
-
-    for i in image:
-        for j in i:
-            if j == 0:
-
-
-
-
-
-
-
-
-print(flipAndInvertImage(image = [[1,1,0],[1,0,1],[0,0,0]]))
