64c271d1132e16cd4648ee66cba3e568263e6227
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-05-12 15:27:35 +0300

    solved

---
+++ binary search/Kth Missing Positive Number(1539).py
@@ -0,0 +1,14 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        if k < arr[0]:
+            return k
+
+        l, r = 0, len(arr) - 1
+        while l <= r:
+            mid = (l + r) // 2
+
+            if arr[mid] - (mid + 1) < k:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l + k
