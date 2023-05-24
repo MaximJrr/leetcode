143bba8a59a64e54e8b83eea1b3ac5f2bc68a74c
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-05-24 15:58:11 +0300

    solved

---
+++ binary search/Minimum Common Value(2540).py
@@ -0,0 +1,8 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        set_1 = set(nums1)
+
+        for i in nums2:
+            if i in set_1:
+                return i
+        return -1
