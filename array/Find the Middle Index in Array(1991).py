6b73d91be9605bba5a4a4058b6eb743fcf4e360e
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-08-27 17:48:50 +0300

    O(n)

---
+++ array/Find the Middle Index in Array(1991).py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+
+        total_sum = sum(nums)
+        left_sum = 0
+
+        for i in range(len(nums)):
+            total_sum -= nums[i]
+
+            if left_sum == total_sum:
+                return i
+
+            left_sum += nums[i]
+
+        return -1
