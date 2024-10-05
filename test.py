64248246e6128881e7be87031441fb60a04e5878
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-10-05 12:11:45 +0300

    test

---
+++ test.py
@@ -0,0 +1,19 @@
+# user = {"name": "Maxim", "last_name": 'Hui'}
+#
+#
+# def test(*args, **kwargs):
+#     return args, kwargs
+#
+# a = ('hello', 'world')
+#
+# t = test(*a, **user)
+#
+# print(t)
+
+
+t = ['keke@mail.ru', 'ghjiuoeregthiuer@gmail.com']
+
+def test(*args):
+    print(*args)
+
+test(t)
